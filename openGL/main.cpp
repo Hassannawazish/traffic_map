867e0da53ba18203cd29e1d9df9ef61b6c319c03
HassanNawazish1 <Hassan-Nawazish.Rasool@automotive-ai.com>
2022-12-28 11:51:54 +0500

    code organization

--- openGL/main.cpp
+++ openGL/main.cpp
@@ -3,6 +3,13 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+void setWindowHints(){
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+}
+
 int main()
 {
     if(!glfwInit()){
@@ -10,11 +17,7 @@ int main()
         return -1;  
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-
+    setWindowHints();
     GLFWwindow* window = glfwCreateWindow(1280, 720, "FBX Visualizer", nullptr, nullptr);
     if(window == nullptr){
         std::cerr<<" GLFW Window Creation Failed........!"<<std::endl;
@@ -34,9 +37,12 @@ int main()
     glClearColor(0.3f,0.3f,0.4f,1.0f);
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glfwSwapBuffers(window);
     }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     std::cout << "OPEN GL "<<std::endl;
     return 0;
