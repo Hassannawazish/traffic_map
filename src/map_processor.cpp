991af968429a55a482ed55c02aba6fd5c902cd95
Hassan Nawazish <50699910+Hassannawazish@users.noreply.github.com>
2023-03-15 18:32:23 +0500

    Right lanes support

--- src/map_processor.cpp
+++ src/map_processor.cpp
@@ -8,8 +8,9 @@ void fill_lane_containers(std::vector <std::map <std::string, double> > &left_la
                         std::vector<double> &left_lane_X2, 
                         std::vector<double> &left_lane_Y2, 
                         std::vector<double> &left_lane_X3, 
-                        std::vector<double> &left_lane_Y3,  
-                        int lane_num){
+                        std::vector<double> &left_lane_Y3,
+                        const std::string flag) 
+{
     double pol = 0.0, pol1 = 0.0, pol2 = 0.0;
     for (int lane_data = 0; lane_data < Config::singleton().planeview_data.size(); lane_data++)
     {
@@ -53,8 +54,8 @@ void fill_lane_containers(std::vector <std::map <std::string, double> > &left_la
                 break;
             }
         }
-
-        if (lane_num == 1 ) {
+        if(flag == "left")
+        {
             left_lane_X1.push_back(planeview_dat["x"] + pol*cos(planeview_dat["hdg"]+(270*(PI/180))));
             left_lane_Y1.push_back(planeview_dat["y"] + pol*sin(planeview_dat["hdg"]+(270*(PI/180))));
 
@@ -63,6 +64,17 @@ void fill_lane_containers(std::vector <std::map <std::string, double> > &left_la
 
             left_lane_X3.push_back(planeview_dat["x"] + (pol + pol1 + pol2)*cos(planeview_dat["hdg"]+(270*(PI/180))));
             left_lane_Y3.push_back(planeview_dat["y"] + (pol + pol1 + pol2)*sin(planeview_dat["hdg"]+(270*(PI/180))));
+        }
+        if(flag == "right")
+        {
+            left_lane_X1.push_back(planeview_dat["x"] - pol*cos(planeview_dat["hdg"]+(270*(PI/180))));
+            left_lane_Y1.push_back(planeview_dat["y"] - pol*sin(planeview_dat["hdg"]+(270*(PI/180))));
+
+            left_lane_X2.push_back(planeview_dat["x"] - (pol + pol1)*cos(planeview_dat["hdg"]+(270*(PI/180))));
+            left_lane_Y2.push_back(planeview_dat["y"] - (pol + pol1)*sin(planeview_dat["hdg"]+(270*(PI/180))));
+
+            left_lane_X3.push_back(planeview_dat["x"] - (pol + pol1 + pol2)*cos(planeview_dat["hdg"]+(270*(PI/180))));
+            left_lane_Y3.push_back(planeview_dat["y"] - (pol + pol1 + pol2)*sin(planeview_dat["hdg"]+(270*(PI/180))));
         }
     }
 }
@@ -79,6 +91,7 @@ map_process::map_process(){
     std::vector <std::map <std::string, double> > right_lane_one = frameRight[1];
     std::vector <std::map <std::string, double> > right_lane_two = frameRight[2];
     std::vector <std::map <std::string, double> > right_lane_three = frameRight[3];
+    std::vector<double> right_lane_X1, right_lane_Y1, right_lane_X2, right_lane_Y2, right_lane_X3, right_lane_Y3;
     
     fill_lane_containers(left_lane_one,
                          left_lane_two,
@@ -89,11 +102,18 @@ map_process::map_process(){
                          left_lane_Y2,
                          left_lane_X3,
                          left_lane_Y3,
-                         1);
-    // fill_lane_containers(right_lane_two, 
-    //                     right_lane_X2,
-    //                     right_lane_Y2,
-    //                     2);
+                         "left");
+    
+    fill_lane_containers(right_lane_one,
+                         right_lane_two,
+                         right_lane_three,
+                         right_lane_X1,
+                         right_lane_Y1,
+                         right_lane_X2,
+                         right_lane_Y2,
+                         right_lane_X3,
+                         right_lane_Y3,
+                         "right");
 
     left_lane.insert(std::pair<std::string, std::vector<double>>("x1", left_lane_X1));
     left_lane.insert(std::pair<std::string, std::vector<double>>("y1", left_lane_Y1));
@@ -101,15 +121,29 @@ map_process::map_process(){
     left_lane.insert(std::pair<std::string, std::vector<double>>("y2", left_lane_Y2));
     left_lane.insert(std::pair<std::string, std::vector<double>>("x3", left_lane_X3));
     left_lane.insert(std::pair<std::string, std::vector<double>>("y3", left_lane_Y3));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("x1", right_lane_X1));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("y1", right_lane_Y1));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("x2", right_lane_X2));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("y2", right_lane_Y2));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("x3", right_lane_X3));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("y3", right_lane_Y3));
     left_lane_X1.clear();
     left_lane_Y1.clear();
     left_lane_X2.clear();
     left_lane_Y2.clear();
     left_lane_X3.clear();
     left_lane_Y3.clear();
+    right_lane_X1.clear();
+    right_lane_Y1.clear();
+    right_lane_X2.clear();
+    right_lane_Y2.clear();
+    right_lane_X3.clear();
+    right_lane_Y3.clear();
 }
 
-std::map< std::string, std::vector<double>> map_process::get_left_lane(int lane_number){
+std::map< std::string, std::vector<double>> map_process::get_lane(int lane_number){
     if(lane_number == 1)
-    return left_lane;
+        return left_lane;
+    if(lane_number == 2)
+        return right_lane;
 }
