2a91fcc4248dcf7510c5c94b0638a279b08729d1
Hassan Nawazish <50699910+Hassannawazish@users.noreply.github.com>
2023-03-15 16:30:59 +0500

    right lanes support

--- src/map_processor.cpp
+++ src/map_processor.cpp
@@ -10,18 +10,10 @@ void fill_lane_containers(std::vector <std::map <std::string, double> > &right_l
                         std::vector<double> &right_lane_X3, 
                         std::vector<double> &right_lane_Y3,  
                         int lane_num){
-    double pol = 0.0;
-    double pol1 = 0.0;
+    double pol = 0.0, pol1 = 0.0, pol2 = 0.0;
     for (int lane_data = 0; lane_data < Config::singleton().planeview_data.size(); lane_data++)
     {
         std::map<std::string, double> planeview_dat = Config::singleton().planeview_data[lane_data];
-        // int kk = 0;
-        // std::map <std::string, double> data = left_lane_one[kk];
-        // if(planeview_dat["s"] == data["sOffset"]){
-        //     double pol = data["a"] + (data["b"]*planeview_dat["length"]) + (data["c"]*pow(2,planeview_dat["length"])) + (data["d"]*pow(3,planeview_dat["length"]));
-        //     kk++;
-        // }        
-
 
         for (int i = 0; i < (right_lane_one.size()-1); i+=2){
             std::map <std::string, double> data = right_lane_one[i];
@@ -48,22 +40,30 @@ void fill_lane_containers(std::vector <std::map <std::string, double> > &right_l
                 break;
             }
         }
+
+        for (int i = 0; i < (right_lane_three.size()-1); i+=2){
+            std::map <std::string, double> data = right_lane_three[i];
+            if(planeview_dat["s"] == data["sOffset"]){
+                pol2 = data["a"] + (data["b"]*planeview_dat["length"]) + (data["c"]*pow(2,planeview_dat["length"])) + (data["d"]*pow(3,planeview_dat["length"]));
+                break;
+            }
+            std::map <std::string, double> data1 = right_lane_three[i+1];
+            if(planeview_dat["s"] == data1["sOffset"]){
+                pol2 = data1["a"] + (data1["b"]*planeview_dat["length"]) + (data1["c"]*pow(2,planeview_dat["length"])) + (data1["d"]*pow(3,planeview_dat["length"]));
+                break;
+            }
+        }
+
         if (lane_num == 1 ) {
             right_lane_X1.push_back(planeview_dat["x"] + pol*cos(planeview_dat["hdg"]+(270*(PI/180))));
             right_lane_Y1.push_back(planeview_dat["y"] + pol*sin(planeview_dat["hdg"]+(270*(PI/180))));
 
             right_lane_X2.push_back(planeview_dat["x"] + (pol + pol1)*cos(planeview_dat["hdg"]+(270*(PI/180))));
             right_lane_Y2.push_back(planeview_dat["y"] + (pol + pol1)*sin(planeview_dat["hdg"]+(270*(PI/180))));
-        }
 
-        // if (lane_num == 1 ) {
-        //     right_lane_X.push_back(planeview_dat["x"] + pol*cos(planeview_dat["hdg"]+(270*(PI/180))));
-        //     right_lane_Y.push_back(planeview_dat["y"] + pol*sin(planeview_dat["hdg"]+(270*(PI/180))));
-        // }
-        // if (lane_num == 2 ) {
-        //     right_lane_X.push_back(planeview_dat["x"] + pol*cos(planeview_dat["hdg"]+(270*(PI/180))));
-        //     right_lane_Y.push_back(planeview_dat["y"] + pol*sin(planeview_dat["hdg"]+(270*(PI/180))));
-        // }
+            right_lane_X3.push_back(planeview_dat["x"] + (pol + pol1 + pol2)*cos(planeview_dat["hdg"]+(270*(PI/180))));
+            right_lane_Y3.push_back(planeview_dat["y"] + (pol + pol1 + pol2)*sin(planeview_dat["hdg"]+(270*(PI/180))));
+        }
     }
 }
 
@@ -94,10 +94,14 @@ map_process::map_process(){
     right_lane.insert(std::pair<std::string, std::vector<double>>("y1", right_lane_Y1));
     right_lane.insert(std::pair<std::string, std::vector<double>>("x2", right_lane_X2));
     right_lane.insert(std::pair<std::string, std::vector<double>>("y2", right_lane_Y2));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("x3", right_lane_X3));
+    right_lane.insert(std::pair<std::string, std::vector<double>>("y3", right_lane_Y3));
     right_lane_X1.clear();
     right_lane_Y1.clear();
     right_lane_X2.clear();
     right_lane_Y2.clear();
+    right_lane_X3.clear();
+    right_lane_Y3.clear();
 }
 
 std::map< std::string, std::vector<double>> map_process::get_right_lane(int lane_number){
